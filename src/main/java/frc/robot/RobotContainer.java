186c87e52a18f8ad2b665462447cf12e15772fe2
Colby Pfeiffer <Colbyjpfeiffer@gmail.com>
2022-04-02 09:50:43 -0400

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants.Buttons;
+import frc.robot.subsystems.Motors;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  public static int id = 1;
+  // The robot's subsystems and commands are defined here...
+  private Motors motors = new Motors();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // Button 2 positive power
+    Buttons.XBOX_Y.whenPressed(new InstantCommand(motors::RunMotorPositive));
+    Buttons.XBOX_Y.whenReleased(new InstantCommand(motors::StopMotor));
+
+    // Button 1 negative power
+    Buttons.XBOX_A.whenPressed(new InstantCommand(motors::RunMotorNegative));
+    Buttons.XBOX_A.whenReleased(new InstantCommand(motors::StopMotor));
+
+    // Button 3 increase selection
+    Buttons.XBOX_B.whenPressed(new InstantCommand(motors::IncreaseID));
+    // Button 4 decrease selectionwwww
+    Buttons.XBOX_X.whenPressed(new InstantCommand(motors::DecreaseID));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return null;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Motors.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.Controllers;
+
+public class Motors extends SubsystemBase {
+  CANSparkMax motor;
+  /** Creates a new Motors. */
+  public Motors() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  private void InitilizeMotor() {
+    if(motor != null) motor.close();
+    motor = new CANSparkMax(RobotContainer.id, MotorType.kBrushless);
+  }
+  public void RunMotorPositive() {
+    if(motor == null) InitilizeMotor();
+    if(RobotContainer.id != motor.getDeviceId()) InitilizeMotor();
+    motor.set(0.2);
+  }
+  public void RunMotorNegative() {
+    if(motor == null) InitilizeMotor();
+    if(RobotContainer.id != motor.getDeviceId()) InitilizeMotor();
+    motor.set(-0.2);
+  }
+  public void StopMotor() {
+    SmartDashboard.putBoolean("Y", Controllers.XBOX.getYButton());
+    SmartDashboard.putBoolean("A", Controllers.XBOX.getAButton());
+    if(Controllers.XBOX.getYButton() || Controllers.XBOX.getAButton()) return;
+    motor.set(0);
+    SmartDashboard.putNumber("Motor", 0);
+  }
+  public void IncreaseID() {
+    RobotContainer.id++;
+    SmartDashboard.putNumber("id", RobotContainer.id);
+  }
+  public void DecreaseID() {
+    if (RobotContainer.id > 1) RobotContainer.id--;
+    SmartDashboard.putNumber("id", RobotContainer.id);
+  }
+}
